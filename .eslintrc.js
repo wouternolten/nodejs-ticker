c00441f8a915261a9428a898ff9c063562ea1a61
Herr Scheissmann <wouternolten@gmail.com>
2020-05-10 16:09:32 +0200

    Add vue to package.json, add linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,27 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es6: true,
     node: true,
   },
   extends: [
-    'airbnb-base',
+    "plugin:vue/essential",
+    "@vue/airbnb",
+    "@vue/typescript/recommended",
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 2020,
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   presets: [
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
         targets: {
-          node: 'current',
+          node: "current",
         },
       },
-      '@babel/preset-typescript',
+      "@vue/cli-plugin-babel/preset",
+      "@babel/preset-typescript",
     ],
   ],
 };
