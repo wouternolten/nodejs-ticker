105018d36d78a5e5ec4b1f4da460d6c13b718e6d
wouter.nolten <wouter.nolten@coolblue.nl>
2020-07-19 11:22:40 +0200

    Remove useless console log

--- etc/routes/symbols/symbolsRoute.ts
+++ etc/routes/symbols/symbolsRoute.ts
@@ -1,10 +1,8 @@
 import { BinanceSymbols } from "../../../src/Infrastructure/Binance/Symbols/BinanceSymbols";
 import * as core from "express-serve-static-core";
-import path from "path";
 
 export function buildRoutes(router: core.Router): void {
   router.get("/symbols", (req: any, res: any) => {
-    console.log(path.relative(process.cwd(), __dirname), __filename, __dirname);
     return new BinanceSymbols()
       .retrieveAll()
       .then((symbols: string[]) => {
