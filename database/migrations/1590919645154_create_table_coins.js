a2143aa05977bfdc8802455cc5db506a7599c7cb
wouter.nolten <wouter.nolten@coolblue.nl>
2020-07-19 09:36:08 +0200

    Change decimal to 5 places after comma

--- database/migrations/1590919645154_create_table_coins.js
+++ database/migrations/1590919645154_create_table_coins.js
@@ -1,13 +1,13 @@
 module.exports = {
   up:
-    "CREATE TABLE tic_coins (" +
-    "id INT NOT NULL AUTO_INCREMENT, " +
-    "symbol VARCHAR(255) NOT NULL, " +
-    "amount DECIMAL NOT NULL, " +
-    "created_at DATETIME NOT NULL, " +
-    "updated_at DATETIME DEFAULT NULL, " +
-    "CONSTRAINT UK_SYMBOL UNIQUE (symbol), " +
-    "PRIMARY KEY (id)" +
-    ");",
+    `CREATE TABLE tic_coins (
+    id INT NOT NULL AUTO_INCREMENT,
+    symbol VARCHAR(255) NOT NULL,
+    amount DECIMAL(14,5) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME DEFAULT NULL,
+    CONSTRAINT UK_SYMBOL UNIQUE (symbol),
+    PRIMARY KEY (id)
+    );`,
   down: "DROP TABLE IF EXISTS tic_coins",
 };
