8e8fa7d5986f7df4d904af421c1f738b6cef7550
wouter.nolten <wouter.nolten@coolblue.nl>
2020-08-01 11:03:26 +0200

    Migrate database to docker

--- database/migrate.js
+++ database/migrate.js
@@ -5,14 +5,23 @@ const dotenv = require("dotenv");
 const { resolve } = require("path");
 
 dotenv.config({ path: resolve(__dirname, "../.env") });
+let password, port;
+
+if(process.env.MIGRATE_INTEGRATION) {
+  password = process.env.INTEGRATION_DATABASE_PASSWORD;
+  port = process.env.INTEGRATION_DATABASE_PORT;
+} else {
+  password = process.env.DATABASE_PASSWORD;
+  port = process.env.DATABASE_PORT
+}
 
 const connection = mysql2.createPool({
   connectionLimit: 10,
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  password,
   database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
+  port,
 });
 
 migration.init(connection, __dirname + "/migrations");
